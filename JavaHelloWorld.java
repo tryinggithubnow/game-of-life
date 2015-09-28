f4344f5d8583a7ac2fd90ca5873d70850caa84c2
tryinggithubnow <qureshi.anees82@gmail.com>
2015-09-28 15:30:25 +0530

    Create JavaHelloWorld.java

---
+++ JavaHelloWorld.java
@@ -0,0 +1,8 @@
+public class JavaHelloWorld
+        {
+                public static void main (String [] args)
+                        {
+                                System.Out.Println("Java Hello World");
+                                System.Out.Println("Hello Again");
+                        }
+        }
